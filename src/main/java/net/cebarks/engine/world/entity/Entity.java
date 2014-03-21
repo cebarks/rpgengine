0d19891dc4a6f9b50264620475dbfdb75768c82c
Anten Skrabec <cebarks@gmail.com>
2014-03-20 18:11:04 -0600

    Refactor to Entity instead of GameObject

--- src/main/java/net/cebarks/engine/world/entity/Entity.java
+++ src/main/java/net/cebarks/engine/world/entity/Entity.java
@@ -1,28 +1,58 @@
 package net.cebarks.engine.world.entity;
 
-import net.cebarks.engine.world.GameObject;
-import net.cebarks.engine.world.Level;
+import java.io.Serializable;
 
-public class Entity extends GameObject {
-	public float xToMove;
-	public float yToMove;
+import net.cebarks.engine.gfx.Animation;
+import net.cebarks.engine.world.World;
 
-	public Entity(Level level) {
-		super(level);
+public class Entity implements Serializable {
+	private static final long serialVersionUID = -5630270584326621892L;
+	private float velocityX;
+	private float velocityY;
+	private float speed;
+	private World world;
+	public float x;
+	public float y;
+	private int id;
+
+	public Entity(World world) {
+		this.world = world;
+		world.addEntity(this);
+		this.id = world.getId();
+		speed = 1f;
 	}
 
 	public void move(int magX, int magY) {
-		xToMove += magX;
-		yToMove += magY;
+		velocityX += magX * speed;
+		velocityY += magY * speed;
 	}
 
-	@Override
-	public void update(double d) {
-		x += xToMove * d;
-		y += yToMove * d;
+	public void setPosition(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 
-		xToMove = 0;
-		yToMove = 0;
-		super.update(d);
+	public void update(double d) {
+		x += velocityX * d;
+		y += velocityY * d;
+
+		velocityX = 0;
+		velocityY = 0;
+	}
+
+	public void onDeath() {
+		getWorld().removeEntity(this);
+	}
+
+	public World getWorld() {
+		return world;
+	}
+
+	public void render() {
+		getAnimation().render(x, y);
+	}
+
+	public Animation getAnimation() {
+		return null;
 	}
 }
